3ca4cbfd43764434ebfe226a04c202147c37e7cd
Ana31198 <anapatriciacancela@gmail.com>
2025-05-15 15:23:31 +0100

    15-05

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -7,13 +7,13 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
-    $response = $this->post('/register', [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
-
+  $response = $this->post('/register', [
+    'name' => 'Test User',
+    'email' => 'test@example.com',
+    'password' => 'password',
+    'password_confirmation' => 'password',
+    'role' => 'responsavel', 
+]);
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
 });
