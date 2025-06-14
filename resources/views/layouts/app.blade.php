6f119c2e7cac3a05def2473224da8630a3d0a78f
Ana31198 <anapatriciacancela@gmail.com>
2025-06-14 22:43:10 +0100

    ee
     teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 @extends('layouts.navigation')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    
+  @section('content')
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+   
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-     
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
     <body class="font-sans antialiased">
+        
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
 
-  @section('content')
           
             @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
